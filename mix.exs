41b695f6af0a8388587ab6af5c34c5cb7ab46619
Joe-noh <goflb.jh@gmail.com>
2017-07-27 23:49:28 +0900

    use poison to treat json

--- mix.exs
+++ mix.exs
@@ -6,14 +6,15 @@ defmodule Tuber.Mixfile do
     version: "0.1.0",
     elixir: "~> 1.5",
     start_permanent: Mix.env == :prod,
-    deps: deps()
+    deps: deps(),
   ]
 
   def application, do: [
-    extra_applications: [:logger]
+    extra_applications: [:logger],
   ]
 
   defp deps, do: [
-    {:httpoison, "~> 0.12"}
+    {:httpoison, "~> 0.12"},
+    {:poison, "~> 3.1"},
   ]
 end
