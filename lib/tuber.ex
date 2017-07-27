e716939ef17eac6847938a6d584d90c5a400760e
Joe-noh <goflb.jh@gmail.com>
2017-07-28 00:52:19 +0900

    functions live dedicated module

--- lib/tuber.ex
+++ lib/tuber.ex
@@ -1,13 +1,2 @@
 defmodule Tuber do
-  @moduledoc """
-  This is the main module to interact with YouTube API.
-  """
-
-  def search(query) do
-    with {:ok, %{body: body}} <- Tuber.Client.get("/search", [], params: [part: "id,snippet", q: query]) do
-      body
-    else
-      {:error, %{reason: reason}} -> {:error, reason}
-    end
-  end
 end
