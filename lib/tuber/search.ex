996520665cc7fa8e6d385ac91f8b5204f3a96bcc
Joe-noh <goflb.jh@gmail.com>
2017-07-29 14:31:30 +0900

    change search API
    should give params as keyword list

--- lib/tuber/search.ex
+++ lib/tuber/search.ex
@@ -1,14 +1,16 @@
 defmodule Tuber.Search do
 
   @doc """
-  Search videos by keyword
+  Search videos
+
+  ref: https://developers.google.com/youtube/v3/docs/search/list?hl=ja
   """
-  @spec list(String.t) :: {:ok, map()} | {:error, :atom}
-  def list(query) do
-    params = [
+  @spec list(Keyword.t) :: {:ok, map()} | {:error, :atom}
+  def list(params) do
+    defaultParams = [
       part: "id,snippet",
-      q: query
     ]
+    params = Keyword.merge(defaultParams, params)
 
     "/search"
     |> Tuber.Client.get([], params: params)
--- test/tuber_test.exs
+++ test/tuber_test.exs
@@ -2,7 +2,4 @@ defmodule TuberTest do
   use ExUnit.Case
   doctest Tuber
 
-  test "greets the world" do
-    assert Tuber.hello() == :world
-  end
 end
